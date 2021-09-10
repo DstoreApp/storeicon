7ed6b0e3f1e3724db6178f4659fe8b75dd1b1ba5
MansourRemadna <almansour.rema2020@gmail.com>
2021-09-10 20:24:58 +0200

    all

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="Description" content="Mansour Programs">
+    <meta property="og:title" content="">
+        <meta property="og:description" content=""> 
+        <meta property="og:image" content="">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:width" content="630">
+        <meta property="og:url" content=""> <!-- مسار الموقع -->
+        <meta property="og:type" content="website">
+    <link rel="icon" href="">
+    <link rel="shortcut" href="">
+    <link rel="apple-touch-icon" href="">
+    <link rel="stylesheet" href="CSS.css">
+</head>
+
+<body>
+<header>
+    <span id="logobox">
+        <img id="logo" src="img/Dstore.svg" alt="Logo">
+    </span>
+    <div id="div-head">
+        <b>
+            <a id="div1_a" href="">Home</a><br>
+            <a id="div1_a" href="">Product</a><br>
+            <a id="div1_a" href="">AboutUs</a><br>
+            <a id="div1_a" href="">Contact</a><br>
+        </b>
+        <select id="divselect">
+            <option><p>Language</p></option>
+            <option><a href=""> Arabic </a></option>
+            <option><a href=""> English </a>  </option>
+            <option><a href=""> Fransh </a>  </option>
+        </select>   
+    </div>
+
+</header>
+<!--------------------------------------------------------------------->
+    <main id="total">
+        <main id="main1">
+            <table id="table1">
+                <tr>
+                    <td>
+                    <h2 id="p1">Notre logiciel et nos icons est à votre disposition</h2>    
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <p id="p2">Nous mettons toutes les applications informatiques et applications Android que nous programmons sur ce site, nous espérons que notre activité vous satisfera .. Nous espérons que vous nous ferez part de vos commentaires et demandes via les sites de réseaux sociaux.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <p id="p3"><b>Il ya des icons, Il nya pas des applications mentenon ^-^ MERCI<b></p>
+                    </td>
+                </tr>
+            </table>
+        </main>
+        <!-- --------------------------------------------------------------- -->
+        <main id="main2">
+            <h2 id="load_h1">Download Icons here</h2>
+            <div id="div_load">
+                <a href="jpg.html" target="_blank"><button id="btnJPG">JPG</button></a>
+                <a href="png.html" target="_blank"><button id="btnPNG">PNG</button></a>
+                <a href="svg.html" target="_blank"><button id="btnSVG">SVG</button></a>
+            </div>
+            <div id="div_main2footer">
+
+            </div>
+        </main>
+        <!--------------------------------------------------------------------->
+        <main id="main3">
+            <center>
+            <table id="table2">
+                <tr>
+                    <td><img id="a0_8x8" src="pic\svg16x16\0_8x8.svg"></td>
+                    <td><img id="1_64x64" src="pic\svg16x16\1_64x64.svg"></td>
+                    <td><img id="2_8x8" src="pic\svg16x16\2_8x8.svg"></td>
+                    <td><img id="3_64x64" src="pic\svg16x16\3_64x64.svg"></td>
+                </tr>
+                <tr>
+                    <td><img id="4_64x64" src="pic\svg16x16\4_64x64.svg"></td>
+                    <td><img id="5_64x64" src="pic\svg16x16\5_16x16.svg"></td>
+                    <td><img id="6_64x64" src="pic\svg16x16\6_64x64.svg"></td>
+                    <td><img id="7_64x64" src="pic\svg16x16\7_64x64.svg"></td>
+                </tr>
+            </table>
+            </center>
+        </main>
+        <!--------------------------------------------------------------------->
+        <article id="footer">
+            <h1></h1>
+        </article>
+    </main>
+<!-- ----------------------------------------------------------------------------- -->
+    <script src="JS.js"></script>
+</body>
+</html>
